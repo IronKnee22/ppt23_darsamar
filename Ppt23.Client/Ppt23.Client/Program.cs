c22235275773a57618ee9b3167afa67840de8b53
Ironknee <marekdarsa@gmail.com>
2023-02-26 19:09:19 +0100

    Oprava pojmenovani

--- Ppt23.Client/Ppt23.Client/Program.cs
+++ Ppt23.Client/Ppt23.Client/Program.cs
@@ -1,6 +1,6 @@
-using Hadacihra;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Ppt23.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
