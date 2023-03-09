d092519142365422adbd0442703b7d95fab21a97
Ironknee <marekdarsa@gmail.com>
2023-03-09 14:23:50 +0100

    zmnv

---
+++ Ppt23.Client/Ppt23.Client/ViewModels/VybaveniVM.cs
@@ -0,0 +1,15 @@
+﻿namespace Ppt23.Client.ViewModels
+{
+    public class VybaveniVM
+    {
+        public string Name { get; set; } = "";
+		//public bool IsNeedRevision { get => ;}
+
+		public bool IsNameJAck { get => Name=="Jack"; }
+
+		public static List<VybaveniVM> VratRandSeznam(int pocet)
+        {
+            return new List<VybaveniVM>() { new VybaveniVM() { Name = "da" }, new VybaveniVM() { Name = "df"} };
+        }
+    }
+}
