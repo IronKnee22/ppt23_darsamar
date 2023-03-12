2ab27499c3be1a9e2d5ed178fe2fbb349512e0c2
Ironknee <marekdarsa@gmail.com>
2023-03-12 20:19:46 +0100

    zmnv

--- Ppt23.Client/Ppt23.Client/ViewModels/VybaveniVM.cs
+++ Ppt23.Client/Ppt23.Client/ViewModels/VybaveniVM.cs
@@ -1,15 +1,62 @@
 ﻿namespace Ppt23.Client.ViewModels
 {
-    public class VybaveniVM
-    {
-        public string Name { get; set; } = "";
-		//public bool IsNeedRevision { get => ;}
+	public class VybaveniVM
+	{
+		Random random = new Random();
+		public string Name { get; set; } = "";
+		public DateTime BuyDate { get; set; }
+		public DateTime LastRevision { get; set; }
+		public bool IsRevisionNeed { get => LastRevision + new TimeSpan(730, 0, 0, 0) < DateTime.Now; }
 
-		public bool IsNameJAck { get => Name=="Jack"; }
+		public VybaveniVM()
+		{
+			this.Name = RandomName();
+			this.BuyDate = RandomDate(new DateTime(2002, 7, 15));
+			this.LastRevision = RandomDate(BuyDate);
+			
 
-		public static List<VybaveniVM> VratRandSeznam(int pocet)
-        {
-            return new List<VybaveniVM>() { new VybaveniVM() { Name = "da" }, new VybaveniVM() { Name = "df"} };
-        }
-    }
+		}
+
+		public static List<VybaveniVM> VratRandSeznam()
+		{
+			Random random = new Random();
+			List<VybaveniVM> ListVybaveni = new List<VybaveniVM>();
+			for (int i = 0; i < random.Next(5, 10); i++)
+			{
+				VybaveniVM vybavenivm = new VybaveniVM();
+
+				ListVybaveni.Add(vybavenivm);
+			}
+			return ListVybaveni;
+
+		}
+
+		public DateTime RandomDate(DateTime since)
+		{
+			int between = (DateTime.Now - since).Days;
+			since.AddDays(random.Next(between));
+
+
+			return since;
+
+
+		}
+
+		public string RandomName()
+		{
+
+
+			string letters = "qwertzuiopasdfghjklyxcvbm";
+			string Name = "";
+
+			int lenght = random.Next(5, 10);
+			for (int i = 0; i < lenght; i++)
+			{
+				int rndpositon = random.Next(letters.Length);
+
+				Name += letters[rndpositon];
+			}
+			return Name;
+		}
+	}
 }
